19597ab4ee9aa8cdb19ce1601caf5e26fdb34b9d
wordpressjeff <me@jeffersonreal.com>
2022-05-19 14:02:12 +0100

    begin converting parallax.js to module

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Index file for all js modules.
+ * 
+ * 
+ * @link https://metabox.io/modernizing-javascript-code-in-wordpress/
+ */
+
+import { parallax } from './parallax.js';
--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,5 @@
 /**
- * Toecaps Parallax
+ * Toecaps Parallax Module.
  *
  * Handle parallax animation using the GSAP library.
  *
@@ -8,7 +8,46 @@
  * @copyright Copyright (c) 2022, Jefferson Real
  */
 
-gsap.registerPlugin( ScrollTrigger );
+const parallax = () => {
+	gsap.registerPlugin( ScrollTrigger );
+
+	const doParallax = () => {
+		const paraElems = document.querySelectorAll( '.parallax' );
+		[...paraElems].forEach((parallax) => {
+			const parallaxInner   = parallax.querySelector( '.parallax_inner' );
+			const parallaxTrigger = parallax.closest( '.parallax_trigger' );
+			const height          = parallaxInner.clientHeight;
+
+			gsap.to( parallaxInner, {
+				y: height / 2,
+				z:0.01,
+				ease: 'none',
+				scrollTrigger: {
+					trigger: parallaxTrigger,
+					scrub: true,
+					start: 'top top', // top of elem meets top of screen
+					end: 'bottom top', // end after scrolling (N)px beyond start.
+				},
+			});
+		});
+	};
+
+	// Poll for doc ready state
+	let docLoaded = setInterval( function () {
+		if ( document.readyState === 'complete' ) {
+			clearInterval( docLoaded );
+			doParallax();
+		}
+	}, 100 );
+};
+
+export { parallax };
+
+
+
+
+
+/*
 
 const parallaxPlugin = () => {
 	gsap.registerPlugin( ScrollTrigger );
@@ -43,3 +82,5 @@ const parallaxPlugin = () => {
 	}, 100 );
 };
 parallaxPlugin();
+
+*/
