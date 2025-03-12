a077bf1e4a89aa2f1cfe1b23b353ac83cbaf8330
BigupJeff <me@jeffersonreal.uk>
2025-03-12 22:06:17 +0000

    update footer backlink

--- footer.php
+++ footer.php
@@ -96,8 +96,8 @@ $tc_street_address = ( isset( $tc_settings['tc_street_address'] ) ) ? $tc_settin
 			?>
 
 			<p class="footer_label">&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?></p>
-			<div style="display:flex;justify-content:center;width:100%;font-family:sans-serif;font-size:1rem;font-weight: 400;">
-				<p id="footer_backlink">Website by <a style="color:#f8f801;text-decoration:none;font-weight:600;letter-spacing:-0.03rem;" target="_blank" href="https://webguyjeff.com/">Web Guy Jeff</a></p>
+			<div id="footer_backlink" aria-label="Visit WebGuyJeff.com for Websites, hosting and domains" title="Visit WebGuyJeff.com for Websites, hosting and domains" style="display: flex; justify-content: center; width: 100%; font-family: sans-serif; font-size: 1rem; font-weight: 400;">
+				<p>Website by <a style="color: #f8f801; text-decoration: none; font-weight: 600; letter-spacing: -0.03rem;" href="https://webguyjeff.com/" target="_blank" rel="noopener">Web Guy Jeff</a></p>
 			</div>
 		</div>
 	</div>
